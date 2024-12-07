918b6ec96817b843fc2271ae25e44a42b5bd25fa
Mateus D Souza <desouza.mateus1996@gmail.com>
2024-12-07 21:51:44 +0100

    Docstring in the scaladoc style and README.

---
+++ src/README.md
@@ -0,0 +1,27 @@
+# Bitcoin Price Analysis with Apache Spark
+
+## Overview
+
+This project was developed to practice the **functional programming paradigm** in creating data processing application with **Scala**. The application processes Bitcoin price data in USD, extracted from the **Binance API**. Each record represents a 1-minute time frame.
+
+The pipeline includes data extraction, transformation, and saving the results as a CSV file. Key features include renaming columns to camel case, formatting timestamps, and calculating derived metrics.
+
+## Features
+
+- **Functional Programming Paradigm**: Pure functions and a transformation pipeline.
+- **Apache Spark**: Scalable data processing.
+- **Data Enrichment**: Adds derived metrics like maximum trade range and positive close flag.
+- **Column Normalization**: Converts column names to camel case.
+- **Date Formatting**: Converts timestamp columns to a human-readable format.
+
+## Project Structure
+
+- `src/main/scala/Main.scala`: Main application logic.
+- Input file: `data/bitcoin-price.csv`
+- Output folder: `output`.
+
+### Prerequisites
+
+1. **Scala**: 2.13.15.
+2. **SBT**: 1.10.6.
+3. **Apache Spark**: 3.5.3.
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -4,14 +4,49 @@ import org.apache.spark.sql.functions.{col, from_unixtime}
 import org.apache.spark.sql.types._
 import org.apache.spark.sql.{Column, DataFrame, SparkSession}
 
+/**
+ * Object responsible for processing Bitcoin price data from a CSV file using Spark.
+ * The pipeline includes reading data, applying transformations, and saving the results.
+ */
 object Main {
+
+  /** Main entry point for the application. */
   def main(args: Array[String]): Unit = {
+
+    /**
+     * Initializes the Spark session for the application.
+     */
     val spark: SparkSession = createSparkSession()
+
+    /**
+     * Path to the input CSV file containing Bitcoin price data.
+     */
     val inputPath = "data/bitcoin-price.csv"
+
+    /**
+     * Path to the output directory where the processed data will be saved.
+     */
     val outputPath = "output/output-data.csv"
 
+    /**
+     * Schema definition for the input CSV file.
+     */
     val inputSchema = defineSchema()
 
+    /**
+     * Pipeline for data processing, implemented using a `for`-comprehension.
+     * Each step applies a transformation to the DataFrame:
+     *  - `readData`: Reads the input data.
+     *  - `renameColumnsToCamelCase`: Converts column names to camelCase format.
+     *  - `formatDateColumns`: Formats specific columns to readable date formats.
+     *  - `addDerivedColumns`: Adds calculated columns to the DataFrame.
+     *
+     * If any step fails (returns `None`) and the pipeline terminates.
+     *
+     * After, matches the result of the pipeline to determine success or failure:
+     *  - If the pipeline succeeds, the resulting DataFrame is displayed and saved to the output path.
+     *  - If the pipeline fails (returns `None`), a failure message is printed to the console.
+     */
     val pipeline = for {
       rawDf <- readData(spark, inputPath, inputSchema)
       camelCaseDf <- Some(renameColumnsToCamelCase(rawDf))
@@ -20,16 +55,15 @@ object Main {
     } yield finalDf
 
     pipeline match {
-      case Some(df) => {
+      case Some(df) =>
         df.show()
         saveData(df, outputPath)
-      }
       case None => println("Pipeline failed")
     }
   }
 
-  /** Creates a Spark session */
-  def createSparkSession(): SparkSession = {
+  /** Creates a Spark session. */
+  private def createSparkSession(): SparkSession = {
     SparkSession.builder()
       .appName("Bitcoin Price Analysis")
       .master("local[*]")
@@ -37,8 +71,8 @@ object Main {
       .getOrCreate()
   }
 
-  /** Defines the schema for the input data */
-  def defineSchema(): StructType = {
+  /** Defines the schema for the input data. */
+  private def defineSchema(): StructType = {
     StructType(Array(
       StructField("Open Time", StringType, nullable = true),
       StructField("Open", DoubleType, nullable = true),
@@ -54,10 +88,16 @@ object Main {
     ))
   }
 
-  /** Reads the data from the input path */
-  def readData(spark: SparkSession, outputPath: String, schema: StructType): Option[DataFrame] = {
+  /** Reads the data from the input path.
+   *
+   * @param spark     Spark Session.
+   * @param inputPath Path to data.
+   * @param schema    Schema of input data.
+   * @return Dataframe extracted.
+   */
+  private def readData(spark: SparkSession, inputPath: String, schema: StructType): Option[DataFrame] = {
     try {
-      Some(spark.read.option("header", value = true).schema(schema).csv(outputPath))
+      Some(spark.read.option("header", value = true).schema(schema).csv(inputPath))
     } catch {
       case e: Exception =>
         println(s"Error reading data: ${e.getMessage}")
@@ -65,36 +105,62 @@ object Main {
     }
   }
 
-  /** Converts column names to camel case */
-  def toCamelCase(column: String): String = {
+  /** Renames all columns in the DataFrame to camel case.
+   *
+   * @param df Dataframe.
+   * @return Dataframe with columns in camel case.
+   */
+  private def renameColumnsToCamelCase(df: DataFrame): DataFrame = {
+    df.columns.foldLeft(df)((tempDF, colName) => tempDF.withColumnRenamed(colName, toCamelCase(colName)))
+  }
+
+  /** Converts column names to camel case.
+   *
+   * @param column column name.
+   * @return Column name in camel case.
+   */
+  private def toCamelCase(column: String): String = {
     column.split("\\s+|_|-").zipWithIndex.map { case (word, index) =>
       if (index == 0) word.toLowerCase else word.capitalize
     }.mkString("")
   }
 
-  /** Renames all columns in the DataFrame to camel case */
-  def renameColumnsToCamelCase(df: DataFrame): DataFrame = {
-    df.columns.foldLeft(df)((tempDF, colName) => tempDF.withColumnRenamed(colName, toCamelCase(colName)))
-  }
-
-  /** Converts a timestamp column from long to a readable date */
-  def toDate(columnName: String): Column = from_unixtime(col(columnName) / 1000)
-
-  /** Formats specified columns as dates */
-  def formatDateColumns(df: DataFrame, columns: Seq[String]): DataFrame = {
+  /** Formats specified columns as dates.
+   *
+   * @param df      Dataframe.
+   * @param columns Sequence of column names.
+   * @return Dataframe with date columns formatted.
+   */
+  private def formatDateColumns(df: DataFrame, columns: Seq[String]): DataFrame = {
     columns.foldLeft(df) { (tempDf, colName) =>
       tempDf.withColumn(colName, toDate(colName))
     }
   }
 
-  /** Adds derived columns for analysis */
-  def addDerivedColumns(df: DataFrame): DataFrame = {
+  /** Converts a timestamp column from long to a readable date.
+   *
+   * @param colName Column name to be transformed.
+   * @return Column with formatted date.
+   */
+  private def toDate(colName: String): Column = from_unixtime(col(colName) / 1000)
+
+  /** Adds derived columns for analysis.
+   *
+   * @param df Dataframe.
+   * @return Dataframe with derived columns.
+   */
+  private def addDerivedColumns(df: DataFrame): DataFrame = {
     df.withColumn("maxTrade", col("high") - col("low"))
       .withColumn("closePositive", col("open") < col("close"))
   }
 
-  /** Saves the transformed DataFrame to the output path */
-  def saveData(df: DataFrame, outputPath: String): Unit = {
+  /** Shows and saves the transformed DataFrame to the output path.
+   *
+   * @param df         Dataframe transformed.
+   * @param outputPath Save destination.
+   */
+  private def saveData(df: DataFrame, outputPath: String): Unit = {
+    df.show()
     try {
       df.repartition(1)
         .write.option("sep", ",")
